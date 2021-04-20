b7c00c7777e96c2e6e9104b6985e56ee474d7d0d
yokekhei <yokekhei@yahoo.com>
2021-04-20 12:21:23 +0800

    Added Selenium keyword option

--- src/portfolio.list.ts
+++ src/portfolio.list.ts
@@ -28,7 +28,7 @@ export const portfolios: Portfolio[] = [
     {
         name: 'CI/CD Deployment for Spring Boot Application',
         desc: 'This is a sample Java Spring Boot web application, built to demonstrate CI/CD pipeline for the continuous deployment of a Spring Boot application and hosting the application on AWS EC2 instance.',
-        keywords: ['Java', 'Spring Boot', 'DevOps', 'Docker', 'Jenkins'],
+        keywords: ['Java', 'Spring Boot', 'Selenium', 'DevOps', 'Docker', 'Jenkins'],
         repository: 'https://github.com/yokekhei/simplilearn_fsd_projects/tree/master/Phase-5/devOps',
         monthCreated: 2,
         yearCreated: 2021
@@ -36,7 +36,7 @@ export const portfolios: Portfolio[] = [
     {
         name: 'Foodbox',
         desc: 'A food ordering system that allows users to manage master data such as food categories, food items and offers, view order and user reports in admin portal, whilst also allow users to view details of food categories and food items, search, filter and sort food items, add food items to cart, perform checkout and make payment in user portal.',
-        keywords: ['Java', 'Spring Boot', 'JPA', 'Hibernate', 'Angular', 'DevOps', 'Docker', 'Jenkins'],
+        keywords: ['Java', 'Spring Boot', 'JPA', 'Hibernate', 'Angular', 'Selenium', 'DevOps', 'Docker', 'Jenkins'],
         repository: 'https://github.com/yokekhei/simplilearn_fsd_projects/tree/master/Capstone/foodBox',
         monthCreated: 3,
         yearCreated: 2021
